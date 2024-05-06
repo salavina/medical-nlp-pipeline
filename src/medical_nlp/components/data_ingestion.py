4b485a15234238e3b4e583562ff00b4b7cea7385
Saeid <saeid.alavi@mail.utoronto.ca>
2024-05-06 06:04:38 -0400

    updated data ingestion component

--- src/medical_nlp/components/data_ingestion.py
+++ src/medical_nlp/components/data_ingestion.py
@@ -3,6 +3,8 @@ from medical_nlp import logger
 from medical_nlp.utils.common import get_size
 import os
 import shutil
+import pandas as pd
+import glob
 from medical_nlp.entity.config_entity import DataIngestionConfig
 
 
@@ -26,6 +28,13 @@ class DataIngestion():
             os.chdir(self.config.root_dir)
             od.download(dataset_url)
             logger.info(f"Downloaded data from {dataset_url} to {str(self.config.root_dir)}")
+            os.chdir('../../')
+            csv_files = glob.glob(str(self.config.root_dir) + '/' +  dataset_url.split('/')[-1] + '/' + '*.csv')
+            base_file = [file for file in csv_files if 'Custom' not in os.path.basename(file)][0]
+            df = pd.read_csv(base_file)
+            df_customzied = df[['Findings', 'Type']]
+            df_customzied.to_csv(str(self.config.root_dir) + '/' +  dataset_url.split('/')[-1] + '/' + os.path.basename(base_file).split('.')[0] + '_Custom.csv' ,index=False)
+            logger.info(f"Saved custom dataset to {str(self.config.root_dir) +  dataset_url.split('/')[-1]}")
         
         except Exception as e:
             raise e
