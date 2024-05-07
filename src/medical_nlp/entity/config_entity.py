eeba1c49cd883e13a5ab6f4ed6e38b3a070d12ad
Saeid <saeid.alavi@mail.utoronto.ca>
2024-05-07 17:02:36 -0400

    added training entity

--- src/medical_nlp/entity/config_entity.py
+++ src/medical_nlp/entity/config_entity.py
@@ -6,3 +6,25 @@ from pathlib import Path
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    nlp_base_model_path: Path
+    nlp_updated_base_model_path: Path
+    params_classes: int
+    
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    nlp_trained_model_path: Path
+    nlp_updated_base_model_path: Path
+    training_data: Path
+    # mlflow_uri: str
+    all_params: dict
+    params_batch_size: int
+    params_epochs: int
+    params_learning_rate: float
+    params_model_name: str
