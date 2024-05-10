dffecb557682b26d6a4c8719b980c3496a245be0
Saeid <saeid.alavi@mail.utoronto.ca>
2024-05-10 12:15:37 -0400

    removed mlflow uri

--- src/medical_nlp/entity/config_entity.py
+++ src/medical_nlp/entity/config_entity.py
@@ -22,7 +22,7 @@ class TrainingConfig:
     nlp_trained_model_path: Path
     nlp_updated_base_model_path: Path
     training_data: Path
-    mlflow_uri: str
+    # mlflow_uri: str
     all_params: dict
     params_batch_size: int
     params_epochs: int
